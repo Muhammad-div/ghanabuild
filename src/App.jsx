0cdfc27c571e2f40f43440689faa5aa73cc96e66
Engineer233 <nii.andrews@gmail.com>
2025-08-02 20:44:09 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
+";
 
 import React, { useState, useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
